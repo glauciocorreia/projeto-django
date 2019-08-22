7faccbe3d5d2441694e8619dee6c7b2b3838c9c2
glauciocorreia <glauciocorreiaa@gmail.com>
2019-08-21 22:24:17 -0300

    014 - CRUD (Delete)

--- clientes/views.py
+++ clientes/views.py
@@ -25,3 +25,13 @@ def persons_update(request, id):
     return redirect('person_list')
   
   return render(request, 'person_form.html', {'form': form})
+
+def persons_delete(request, id):
+  person = get_object_or_404(Person, pk=id)
+  form = PersonForm(request.POST or None, request.FILES or None, instance=person)
+
+  if request.method == 'POST':
+    person.delete()
+    return redirect('person_list')
+  
+  return render(request, 'person_delete.html', {'person': person})
