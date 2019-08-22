a76deb9bcbd5340fc468784e88a2c2f79ccb3d31
glauciocorreia <glauciocorreiaa@gmail.com>
2019-08-21 21:48:27 -0300

    012 - CRUD (Create)

--- clientes/urls.py
+++ clientes/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import persons_list
+from .views import persons_new
 
 urlpatterns = [
-    path('list/', persons_list),
+    path('list/', persons_list, name="person_list"),
+    path('new/', persons_new, name="person_new"),
 ]
